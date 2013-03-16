6d98b16454fe26f829c80ae5dace48817159f2ea
Evgeny Goldin <evgenyg@gmail.com>
2013-03-16 14:28:57 +0100

    "run-curl.sh" update - only first 3 lines of every response are displayed

--- run-curl.sh
+++ run-curl.sh
@@ -4,40 +4,40 @@ set -e
 set -x
 
 echo hello-js:
-curl http://127.0.0.1:3051
+curl http://127.0.0.1:3051 | head -3
 
 echo hello-js-configs-new-keys:
-curl http://127.0.0.1:3025
+curl http://127.0.0.1:3025 | head -3
 
 echo nirc:
-curl http://127.0.0.1:3048
+curl http://127.0.0.1:3048 | head -3
 
 echo hello-js-configs-update-keys:
-curl http://127.0.0.1:3018
+curl http://127.0.0.1:3018 | head -3
 
 echo hello-coffee-port-number:
-curl http://127.0.0.1:3086
+curl http://127.0.0.1:3086 | head -3
 
 echo hello-js-port-number:
-curl http://127.0.0.1:3033
+curl http://127.0.0.1:3033 | head -3
 
 echo nodejs-blackboard:
-curl http://127.0.0.1:3095
+curl http://127.0.0.1:3095 | head -3
 
 echo nodejs-blackboard Mongo-1:
-curl http://127.0.0.1:27017
+curl http://127.0.0.1:27017 | head -3
 
 echo nodejs-blackboard Mongo-2:
-curl http://127.0.0.1:28017
+curl http://127.0.0.1:28017 | head -3
 
 echo uptime:
-curl http://127.0.0.1:3036
+curl http://127.0.0.1:3036 | head -3
 
 echo uptime Mongo-1:
-curl http://127.0.0.1:27018
+curl http://127.0.0.1:27018 | head -3
 
 echo uptime Mongo-2:
-curl http://127.0.0.1:28018
+curl http://127.0.0.1:28018 | head -3
 
 echo We are good
 exit 0
