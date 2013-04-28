fbb67a39d4289c7b84c7ed82a4474c6b95c4e1fc
Evgeny Goldin <evgenyg@gmail.com>
2013-04-28 20:15:37 +0200

    run-cleanup.sh runs 'stopall'

--- run-cleanup.sh
+++ run-cleanup.sh
@@ -2,5 +2,5 @@
 
 git checkout -f
 git clean    -df
-./gradlew -i clean cleanModules setup -DcleanNode
+./gradlew -i clean cleanModules stopall -DcleanNode
 echo Current directory space = [`du -hs . | awk '{ print $1 }'`]
