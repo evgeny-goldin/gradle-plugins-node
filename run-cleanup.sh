51644e8d213155295d12bfd3027e81dbed21ce0c
Evgeny Goldin <evgenyg@gmail.com>
2013-04-29 01:24:25 +0200

    run-cleanup.sh accepts an argument

--- run-cleanup.sh
+++ run-cleanup.sh
@@ -2,5 +2,5 @@
 
 git checkout -f
 git clean    -df
-./gradlew -i clean cleanModules stop -DcleanNode
+./gradlew $1 clean cleanModules stop -DcleanNode
 echo Current directory space = [`du -hs . | awk '{ print $1 }'`]
