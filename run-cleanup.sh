ce1b0f021af581b617589fe41f922aba8e51d4cc
Evgeny Goldin <evgenyg@gmail.com>
2013-04-29 18:01:48 +0200

    run-cleanup.sh - set -e/-x added

--- run-cleanup.sh
+++ run-cleanup.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+set -e
+set -x
+
 git checkout -f
 git clean    -df
+
 ./gradlew -S --refresh-dependencies $1 clean cleanModules stop -DcleanNode
+
 echo Current directory space = [`du -hs . | awk '{ print $1 }'`]
