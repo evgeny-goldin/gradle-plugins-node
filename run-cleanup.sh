c640dd8f666a5321523f0f70af9816842d2915bd
Evgeny Goldin <evgenyg@gmail.com>
2013-04-29 17:59:55 +0200

    run-cleanup.sh update: '-S --refresh-dependencies'

--- run-cleanup.sh
+++ run-cleanup.sh
@@ -2,5 +2,5 @@
 
 git checkout -f
 git clean    -df
-./gradlew $1 clean cleanModules stop -DcleanNode
+./gradlew -S --refresh-dependencies $1 clean cleanModules stop -DcleanNode
 echo Current directory space = [`du -hs . | awk '{ print $1 }'`]
