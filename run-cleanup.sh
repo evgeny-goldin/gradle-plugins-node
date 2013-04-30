150eadeac24235ad8e1f44c794ba58b5ed27a21e
Evgeny Goldin <evgenyg@gmail.com>
2013-04-30 17:14:11 +0200

    run-cleanup.sh - '-PpluginsVersion' argument added

--- run-cleanup.sh
+++ run-cleanup.sh
@@ -6,6 +6,6 @@ set -x
 git checkout -f
 git clean    -df
 
-./gradlew -S --refresh-dependencies $1 clean cleanModules stop -DcleanNode
+./gradlew -S --refresh-dependencies $1 clean cleanModules stop -DcleanNode -PpluginsVersion=$2
 
 echo Current directory space = [`du -hs . | awk '{ print $1 }'`]
