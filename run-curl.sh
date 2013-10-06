ad9538959dbb02f2f3e9cb7965758937a60b9067
Evgeny Goldin <evgenyg@gmail.com>
2013-10-06 17:59:28 +0200

    Curl update - '-if' added

--- run-curl.sh
+++ run-curl.sh
@@ -4,43 +4,43 @@ set -e
 set -x
 
 echo hello-coffee-port-number:
-curl http://127.0.0.1:3086
+curl -if http://127.0.0.1:3086
 
 echo hello-js:
-curl http://127.0.0.1:3051
+curl -if http://127.0.0.1:3051
 
 echo hello-js-configs-new-keys:
-curl http://127.0.0.1:3025
+curl -if http://127.0.0.1:3025
 
 echo hello-js-configs-update-keys:
-curl http://127.0.0.1:3018
+curl -if http://127.0.0.1:3018
 
 echo hello-js-port-number:
-curl http://127.0.0.1:3033
+curl -if http://127.0.0.1:3033
 
 echo nirc:
-curl http://127.0.0.1:3048
+curl -if http://127.0.0.1:3048
 
 echo nodejs-blackboard:
-curl http://127.0.0.1:3095
+curl -if http://127.0.0.1:3095
 
 echo nodejs-blackboard Mongo-1:
-curl http://127.0.0.1:27017
+curl -if http://127.0.0.1:27017
 
 echo nodejs-blackboard Mongo-2:
-curl http://127.0.0.1:28017
+curl -if http://127.0.0.1:28017
 
 echo redis:
-curl http://127.0.0.1:3055
+curl -if http://127.0.0.1:3055
 
 echo uptime:
-curl http://127.0.0.1:3036
+curl -if http://127.0.0.1:3036
 
 echo uptime Mongo-1:
-curl http://127.0.0.1:27018
+curl -if http://127.0.0.1:27018
 
 echo uptime Mongo-2:
-curl http://127.0.0.1:28018
+curl -if http://127.0.0.1:28018
 
 echo We are good!
 exit 0
